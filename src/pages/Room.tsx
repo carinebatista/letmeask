a241876734f98e871765cea01ec8123441177313
carinebatista <carinedcb@hotmail.com>
2021-06-22 23:45:30 -0300

    Change te method to on (Every time user add a Question) firebase catch the data Dynamic

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -43,7 +43,7 @@ export function Room(){
   useEffect(() => {
     const roomRef= database.ref(`rooms/${roomId}`);
 
-    roomRef.once('value', room => {
+    roomRef.on('value', room => {
       const databaseRoom = room.val()
       const FirebaseQuestions: FirebaseQuestions = databaseRoom.questions ?? {}
 
